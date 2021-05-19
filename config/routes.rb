0da86c5a796a83e4b31fb3c52626135fa67699f9
amira <amiraabouhadid@gmail.com>
2021-05-19 13:43:27 +0200

    Add friendships routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-
   root 'posts#index'
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :create] do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
+  resources :users, only: %i[index show] do
+    resources :friendships, only: %i[create accept destroy]
+    get '/accept_friendship', to: 'friendship#accept'
   end
+  resources :posts, only: %i[index create] do
+    resources :comments, only: [:create]
+    resources :likes, only: %i[create destroy]
+  end
+  resources :friendships
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
