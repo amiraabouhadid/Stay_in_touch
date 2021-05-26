d05a5ab53bf57236a4734b9cc92ae7479ddd007a
amira <amiraabouhadid@gmail.com>
2021-05-26 15:16:30 +0200

    Fix styling and user ability to add friend.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,12 @@
 class FriendshipsController < ApplicationController
   def create
     friend_id = params[:user][:invitee_id]
-    # render json: params
-    h1 = { user_id: current_user.id, friend_id: friend_id, initiator_id: current_user.id, confirmed: nil }
-    h2 = { user_id: friend_id, friend_id: current_user.id, initiator_id: current_user.id, confirmed: nil }
+    u1 = { user_id: current_user.id, friend_id: friend_id, initiator_id: current_user.id, confirmed: nil }
+    u2 = { user_id: friend_id, friend_id: current_user.id, initiator_id: current_user.id, confirmed: nil }
     begin
       ActiveRecord::Base.transaction do
-        Friendship.create!(h1)
-        Friendship.create!(h2)
+        Friendship.create!(u1)
+        Friendship.create!(u2)
       end
       redirect_to users_path, notice: 'Request sent'
     rescue ActiveRecord::Rollback
